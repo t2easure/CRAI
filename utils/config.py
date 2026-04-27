479382737611dbcd9f0248dc0e58a43e263ef94e
t2easure <tjduswl8@gmail.com>
2026-04-27 19:01:06 +0900

    PostgreSQL 환경변수 및 psycopg2 패키지 추가

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone, timedelta
 load_dotenv()
 
 APIFY_API_TOKEN = os.getenv("APIFY_API_TOKEN", "")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 
 # Data output directory
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
